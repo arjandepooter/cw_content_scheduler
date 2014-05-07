24a2de36aa9047c74eb44c8dc17e98ff90015d22
Leon de Rijke <leon@cmsworks.nl>
2014-05-07 10:05:41 +0200

    Release 0.1.0 to TER

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,18 @@
 <?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "cw_content_scheduler".
+ *
+ * Auto generated 07-05-2014 10:04
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Content Scheduler',
-	'description' => '',
+	'description' => 'Publish content on a repeating schedule. Ever wanted to show a banner "Every Tuesday"? Now you can!',
 	'category' => 'plugin',
 	'author' => 'Arjan de Pooter',
 	'author_email' => 'arjan@cmsworks.nl',
@@ -11,21 +22,22 @@ $EM_CONF[$_EXTKEY] = array(
 	'module' => '',
 	'state' => 'alpha',
 	'internal' => '',
-	'uploadfolder' => '0',
+	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '',
+	'version' => '0.1.0',
 	'constraints' => array(
 		'depends' => array(
-			'cms' => '4.7.0-4.7.999',
+			'typo3' => '4.7.0-4.7.999',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
+	'_md5_values_when_last_written' => 'a:17:{s:21:"ext_conf_template.txt";s:4:"5d36";s:17:"ext_localconf.php";s:4:"4689";s:14:"ext_tables.php";s:4:"e7de";s:14:"ext_tables.sql";s:4:"0dce";s:9:"README.md";s:4:"5d99";s:35:"Classes/Hooks/EnableColumnsHook.php";s:4:"2533";s:26:"Documentation/Includes.txt";s:4:"6d5f";s:23:"Documentation/Index.rst";s:4:"4f06";s:26:"Documentation/Settings.yml";s:4:"9c51";s:44:"Documentation/AdministratorsManual/Index.rst";s:4:"5759";s:40:"Documentation/DevelopersCorner/Index.rst";s:4:"311d";s:32:"Documentation/Feedback/Index.rst";s:4:"b0b0";s:36:"Documentation/Introduction/Index.rst";s:4:"0557";s:35:"Documentation/UsersManual/Index.rst";s:4:"0856";s:40:"Resources/Private/Language/locallang.xlf";s:4:"83ef";s:43:"Resources/Private/Language/nl.locallang.xlf";s:4:"07c3";s:44:"Resources/Private/Queries/ScheduledQuery.sql";s:4:"0b82";}',
 );
 
 ?>
