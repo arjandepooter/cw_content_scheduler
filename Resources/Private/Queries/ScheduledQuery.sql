e242785cc6e4d6d870cf6710b7a321d1ce9cd19b
Arjan de Pooter <arjan@cmsworks.nl>
2014-05-08 13:39:17 +0200

    Query failed for future dates, now fixed

--- Resources/Private/Queries/ScheduledQuery.sql
+++ Resources/Private/Queries/ScheduledQuery.sql
@@ -1,7 +1,8 @@
  AND IF(
 	NOT(%1$s.%5$s = '')
 	AND %1$s.%4$s > 0
-	AND %1$s.%3$s > 0,
+	AND %1$s.%3$s > 0
+	AND %1$s.%2$s < %6$d,
 	(%6$d - %1$s.%2$s) DIV
 	(%1$s.%4$s * (CASE %1$s.%5$s
 		WHEN 'm' THEN 60
@@ -10,7 +11,7 @@
 		WHEN 'w' THEN 7*24*60*60
 		ELSE 1
 	END)) <>
-	(%6$d - %1$s.%3$s) DIV
+	(%6$d - CAST(%1$s.%3$s AS SIGNED)) DIV
 	(%1$s.%4$s * (CASE %1$s.%5$s
 		WHEN 'm' THEN 60
 		WHEN 'h' THEN 60*60
