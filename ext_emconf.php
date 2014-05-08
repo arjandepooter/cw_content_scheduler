af6dfff2197b90f407d079b7c2f49eaac7cc1209
Leon de Rijke <leon@cmsworks.nl>
2014-05-08 13:43:27 +0200

    Release 0.3.1 to TER

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "cw_content_scheduler".
  *
- * Auto generated 07-05-2014 14:54
+ * Auto generated 08-05-2014 13:41
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -27,7 +27,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '0.3.0',
+	'version' => '0.3.1',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.7.0-4.7.999',
@@ -37,7 +37,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:20:{s:21:"ext_conf_template.txt";s:4:"5d36";s:12:"ext_icon.gif";s:4:"7f3e";s:12:"ext_icon.png";s:4:"477f";s:17:"ext_localconf.php";s:4:"0922";s:14:"ext_tables.php";s:4:"2434";s:14:"ext_tables.sql";s:4:"0dce";s:9:"README.md";s:4:"5d99";s:27:"Classes/Hooks/CmsLayout.php";s:4:"5ac1";s:35:"Classes/Hooks/EnableColumnsHook.php";s:4:"2533";s:26:"Documentation/Includes.txt";s:4:"6d5f";s:23:"Documentation/Index.rst";s:4:"4f06";s:26:"Documentation/Settings.yml";s:4:"263c";s:44:"Documentation/AdministratorsManual/Index.rst";s:4:"5759";s:40:"Documentation/DevelopersCorner/Index.rst";s:4:"311d";s:32:"Documentation/Feedback/Index.rst";s:4:"b0b0";s:36:"Documentation/Introduction/Index.rst";s:4:"0557";s:35:"Documentation/UsersManual/Index.rst";s:4:"0856";s:40:"Resources/Private/Language/locallang.xlf";s:4:"7a27";s:43:"Resources/Private/Language/nl.locallang.xlf";s:4:"3d36";s:44:"Resources/Private/Queries/ScheduledQuery.sql";s:4:"0b82";}',
+	'_md5_values_when_last_written' => 'a:20:{s:21:"ext_conf_template.txt";s:4:"5d36";s:12:"ext_icon.gif";s:4:"7f3e";s:12:"ext_icon.png";s:4:"477f";s:17:"ext_localconf.php";s:4:"63f9";s:14:"ext_tables.php";s:4:"136f";s:14:"ext_tables.sql";s:4:"0dce";s:9:"README.md";s:4:"5d99";s:27:"Classes/Hooks/CmsLayout.php";s:4:"5ac1";s:35:"Classes/Hooks/EnableColumnsHook.php";s:4:"2533";s:26:"Documentation/Includes.txt";s:4:"6d5f";s:23:"Documentation/Index.rst";s:4:"4f06";s:26:"Documentation/Settings.yml";s:4:"90eb";s:44:"Documentation/AdministratorsManual/Index.rst";s:4:"5759";s:40:"Documentation/DevelopersCorner/Index.rst";s:4:"311d";s:32:"Documentation/Feedback/Index.rst";s:4:"b0b0";s:36:"Documentation/Introduction/Index.rst";s:4:"0557";s:35:"Documentation/UsersManual/Index.rst";s:4:"0856";s:40:"Resources/Private/Language/locallang.xlf";s:4:"7a27";s:43:"Resources/Private/Language/nl.locallang.xlf";s:4:"3d36";s:44:"Resources/Private/Queries/ScheduledQuery.sql";s:4:"af8c";}',
 );
 
 ?>
